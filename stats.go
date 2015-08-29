fff9a43cabd36ebe1326c0b82b75980c36871c19
istdeo <istdeosingh@gmail.com>
2015-08-29 12:44:41 +0530

    removed prints

--- stats.go
+++ stats.go
@@ -10,12 +10,10 @@ import (
 func CDF(xInput vlib.VectorF) (xOut, yOut vlib.VectorF) {
 	n := xInput.Size()
 	str := fmt.Sprintf("%f:%f:1", 1/float64(n), 1/float64(n))
-	fmt.Println(str, n)
 	yOut = vlib.ToVectorF(str)
 	if yOut[yOut.Size()-1] < 1 {
 		yOut = yOut.Insert(yOut.Size()-1, 1)
 	}
-	fmt.Println(yOut[0], yOut[yOut.Size()-1], yOut.Size())
 	sort.Float64s([]float64(xInput))
 	xOut = xInput
 	// xx := vlib.NewVectorI(n)
@@ -28,7 +26,6 @@ func CDF(xInput vlib.VectorF) (xOut, yOut vlib.VectorF) {
 		}
 	}
 	yOut = yOut.Insert(0, 0)
-	fmt.Println(yOut[0], yOut[yOut.Size()-1], yOut.Size())
 	xOut1 := vlib.NewVectorF(2 * n)
 	yOut1 := vlib.NewVectorF(2 * n)
 	// fmt.Printf("\n %v \n", yOut)
